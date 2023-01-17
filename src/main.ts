6665dee44a2e123896dbfdd74c1da266a940b7f6
J-hon <Jonadify007@gmail.com>
2023-01-17 14:36:27 +0100

    Error handling: Adds HttpExceptionFilter

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './modules/common/http-exception.filter';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get('PORT');
+
+  app.useGlobalFilters(new HttpExceptionFilter());
 
   await app.listen(port);
 }
